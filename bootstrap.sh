de3697a4a40c7f9cd6704927c65ab73b3afad4d2
Marcos Tapajós <tapajos@gmail.com>
2012-06-16 20:55:41 -0300

    Add pryrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,10 @@ git pull
 
 rm -f "$HOME/.bash_profile"
 rm -f "$HOME/.bashrc"
+rm -f "$HOME/.pryrc"
 
 ln -s "$(pwd)/bash_profile" "$HOME/.bash_profile"
 ln -s "$(pwd)/bashrc" "$HOME/.bashrc"
+ln -s "$(pwd)/pryrc" "$HOME/.pryrc"
 
 source ~/.bash_profile
