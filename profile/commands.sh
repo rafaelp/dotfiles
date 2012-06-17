4dafdee1aac9679feb90a45b262d39b2cc6effdb
Marcos Tapajós <tapajos@gmail.com>
2012-06-16 21:14:53 -0300

    Remove Kindle alias

--- profile/commands.sh
+++ profile/commands.sh
@@ -2,5 +2,4 @@
 alias cd..="cd .."
 alias dns="dscacheutil -flushcache"
 alias ls="ls -h"
-alias kindle="rsync -av --delete /Volumes/Kindle $HOME/Documents/"
 alias f1="rsync -av $HOME/Dropbox/F1/ $HOME/Pictures/Fotos/Carros/F1/"
