4e07b92fd8611a3fccfc66d35b49470f8e3e993b
Marcos Tapajós <tapajos@gmail.com>
2013-04-02 22:41:09 -0300

    Add rvmrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 git pull
 
-for FILE in bash_profile pryrc tm_properties gitconfig irbrc my.cnf
+for FILE in bash_profile pryrc tm_properties gitconfig irbrc my.cnf rvmrc
 do
 	rm -f "$HOME/.$FILE"
 	ln -s "$(pwd)/$FILE" "$HOME/.$FILE"
