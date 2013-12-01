55b3f5b9039ad4e6b4e0e84c4355bc4ef5ecaf73
Rafael Lima <contato@rafael.adm.br>
2013-12-01 19:12:16 -0200

    adds gemrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 git pull
 
-for FILE in bash_profile pryrc tm_properties gitconfig irbrc my.cnf
+for FILE in bash_profile pryrc tm_properties gitconfig irbrc my.cnf gemrc
 do
 	rm -f "$HOME/.$FILE"
 	ln -s "$(pwd)/$FILE" "$HOME/.$FILE"
