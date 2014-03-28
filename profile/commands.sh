a74e064aeb623a2f68e39bb7ae4c32f2d5f5cafd
Rafael Lima <contato@rafael.adm.br>
2014-03-28 16:13:00 -0300

    adds alias update to update software

--- profile/commands.sh
+++ profile/commands.sh
@@ -3,3 +3,4 @@ alias dns="sudo killall -HUP mDNSResponder"
 # Before Lion
 # alias dns="dscacheutil -flushcache"
 alias ls="ls -h"
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; rvm get latest --auto-dotfiles'
